a675d3528a3c7e3ea64d3c135b2d8c2c22cfb9d0
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-06-08 21:27:48 -0300

    add theme extensions

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,25 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'var(--font-inter)',
+      },
+      colors: {
+        gray1: '#2d2f31',
+        gray2: '#3e4143',
+        gray3: '#6a6f73',
+        gray4: '#d1d7dc',
+      },
+    },
+  },
   plugins: [],
-};
+}
 
-export default config;
+export default config
