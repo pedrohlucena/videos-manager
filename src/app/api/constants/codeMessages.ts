a485e2c6497393f61ec2f99d30cdccd4a304af8b
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-06-15 17:23:13 -0300

    add code messages

---
+++ src/app/api/constants/codeMessages.ts
@@ -0,0 +1,10 @@
+export const CODE_MESSAGES = {
+  INTERNAL_SERVER_ERROR: {
+    code: '001',
+    message: 'Internal server error',
+  },
+  PLAYLIST_NOT_FOUND: {
+    code: '002',
+    message: 'Playlist not found',
+  },
+}
---
+++ src/app/api/constants/index.ts
@@ -0,0 +1,1 @@
+export { CODE_MESSAGES } from './codeMessages'
