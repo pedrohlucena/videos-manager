1ceac31619169bbf4a2714d1bcb1b166de01fa2a
Pedro H. Lucena <pedrohlucena413@gmail.com>
2024-06-15 18:12:12 -0300

    add /api/bff/playlists/:playlistId/videos call on video management

--- src/app/manage/playlist/[playlistId]/video/[videoId]/page.tsx
+++ src/app/manage/playlist/[playlistId]/video/[videoId]/page.tsx
@@ -1,4 +1,6 @@
 import { YoutubeEmbed } from '@/components'
+import { env } from '@/env'
+import { Video } from '@/models'
 import { PlaylistContent } from './components'
 
 interface VideoManagementProps {
@@ -8,8 +10,26 @@ interface VideoManagementProps {
   }
 }
 
-export default function VideoManagement({ params }: VideoManagementProps) {
+async function getVideos(playlistId: string): Promise<Video[]> {
+  const url = `${env.NEXT_PUBLIC_API_BASE_URL}/api/bff/playlists/${playlistId}/videos`
+
+  const response = await fetch(url, {
+    next: {
+      revalidate: 60 * 60, // 9 hours
+    },
+  })
+
+  const { items } = await response.json()
+
+  return items
+}
+
+export default async function VideoManagement({
+  params,
+}: VideoManagementProps) {
   const { playlistId, videoId } = params
+
+  const videos = await getVideos(playlistId)
 
   return (
     <div className="flex relative">
@@ -17,7 +37,7 @@ export default function VideoManagement({ params }: VideoManagementProps) {
         <YoutubeEmbed videoId={videoId} />
       </main>
 
-      <PlaylistContent />
+      <PlaylistContent videos={videos} />
     </div>
   )
 }
